a6b9c109d585b6185cc3003c8aa1e77daf438fa5
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-02-27 04:17:09 +0900

    update response type of express

--- packages/backend/src/@types/express-user.d.ts
+++ packages/backend/src/@types/express-user.d.ts
@@ -1,7 +1,13 @@
+import { Request } from 'express';
+
 export declare global {
   namespace Express {
     interface User {
       uid: string;
     }
   }
+}
+
+export declare module 'express' {
+  export type AuthenticatedRequest = Express.AuthenticatedRequest & Request;
 }
