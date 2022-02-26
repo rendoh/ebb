dea09d3c82ca4d121ff6f5bb72598fcf207eb634
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-02-27 04:16:26 +0900

    add global validation pipe

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { PrismaClientExceptionFilter } from './prisma/prisma-client-exception.filter';
 
@@ -15,6 +15,10 @@ import { PrismaClientExceptionFilter } from './prisma/prisma-client-exception.fi
     {
       provide: APP_GUARD,
       useClass: AuthGuard('bearer'),
+    },
+    {
+      provide: APP_PIPE,
+      useFactory: () => new ValidationPipe({ whitelist: true }),
     },
     {
       provide: APP_FILTER,
