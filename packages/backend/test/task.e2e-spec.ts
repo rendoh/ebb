390c6d150c6d857feeee940ad166df8de1eed394
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-02-27 17:06:50 +0900

    create e2e test for task

---
+++ packages/backend/test/task.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from '../src/app.module';
+
+describe('TaskController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  describe('GET: /tasks', () => {
+    it('認証前のユーザはアクセスできない', () => {
+      return request(app.getHttpServer()).get('/tasks').expect(401);
+    });
+  });
+});
