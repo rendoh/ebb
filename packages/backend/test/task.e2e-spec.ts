a6782e18c07d22e2334d1ec2d8d1863221814442
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-02-27 23:40:58 +0900

    wip: setup e2e testing with firebase local emulator

--- packages/backend/test/task.e2e-spec.ts
+++ packages/backend/test/task.e2e-spec.ts
@@ -2,11 +2,36 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
+import axios from 'axios';
+
+type User = {
+  kind: string;
+  localId: string;
+  email: string;
+  idToken: string;
+  refreshToken: string;
+  expiresIn: string;
+};
 
 describe('TaskController (e2e)', () => {
   let app: INestApplication;
+  let user: User;
 
   beforeAll(async () => {
+    /**
+     * TODO:
+     * @faker-js/faker などに変更
+     * 実装方法をリファクタリング
+     */
+    const { data } = await axios.post<User>(
+      'http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signUp?key=local',
+      {
+        email: `user_${Math.random()}@example.com`,
+        password: 'P@ssw0rd',
+      },
+    );
+    user = data;
+
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -18,6 +43,12 @@ describe('TaskController (e2e)', () => {
   describe('GET: /tasks', () => {
     test('認証前のユーザはアクセスできない', () => {
       return request(app.getHttpServer()).get('/tasks').expect(401);
+    });
+    test('認証済のユーザは自分のTask一覧を取得できる', () => {
+      return request(app.getHttpServer())
+        .get('/tasks')
+        .auth(user.idToken, { type: 'bearer' })
+        .expect(200);
     });
   });
 });
