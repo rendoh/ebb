2f6ab10d4b3d60a06f65d603b095213b7e9457f8
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-03-01 23:21:27 +0900

    fix validation rules

--- packages/backend/src/tasks/dto/create-task.dto.ts
+++ packages/backend/src/tasks/dto/create-task.dto.ts
@@ -1,11 +1,13 @@
-import { IsString, MaxLength } from 'class-validator';
+import { IsString, MinLength, MaxLength } from 'class-validator';
 
 export class CreateTaskDto {
   @IsString()
+  @MinLength(1)
   @MaxLength(256)
   readonly title!: string;
 
   @IsString()
+  @MinLength(1)
   @MaxLength(1024)
   readonly content!: string;
 }
