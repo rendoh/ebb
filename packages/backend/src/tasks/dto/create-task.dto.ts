b35a2e7f1576cf5c7bbfeaf8b74ac201e5d976c4
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-03-01 20:25:27 +0900

    use swagger cli plugin

--- packages/backend/src/tasks/dto/create-task.dto.ts
+++ packages/backend/src/tasks/dto/create-task.dto.ts
@@ -1,13 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MaxLength } from 'class-validator';
 
 export class CreateTaskDto {
-  @ApiProperty()
   @IsString()
   @MaxLength(256)
   readonly title!: string;
 
-  @ApiProperty()
   @IsString()
   @MaxLength(1024)
   readonly content!: string;
--- packages/backend/src/tasks/dto/task.dto.ts
+++ packages/backend/src/tasks/dto/task.dto.ts
@@ -1,21 +1,8 @@
-import { ApiProperty } from '@nestjs/swagger';
-
 export class TaskDto {
-  @ApiProperty()
   id!: string;
-
-  @ApiProperty()
   title!: string;
-
-  @ApiProperty()
   content!: string;
-
-  @ApiProperty()
   createdAt!: Date;
-
-  @ApiProperty()
   updatedAt!: Date;
-
-  @ApiProperty()
   archived!: boolean;
 }
