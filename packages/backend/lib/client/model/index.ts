0c80be9ed904f9824608231b752e11dcac4a06a8
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-03-02 01:46:56 +0900

    add api client generated by openapi generator

---
+++ packages/backend/lib/client/model/index.ts
@@ -0,0 +1,6 @@
+export * from './create-task-dto';
+export * from './paginated-dto';
+export * from './pagination-query-dto';
+export * from './task-dto';
+export * from './update-task-dto';
+export * from './validation-error-dto';
