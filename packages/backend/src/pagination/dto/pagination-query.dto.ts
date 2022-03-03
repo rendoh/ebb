a69f9bd1e89d0b1289febbffa37a7b5865496860
Ryohei Endo <r-endo@studio-spoon.co.jp>
2022-03-04 01:32:23 +0900

    fix pagination query

--- packages/backend/src/pagination/dto/pagination-query.dto.ts
+++ packages/backend/src/pagination/dto/pagination-query.dto.ts
@@ -1,20 +1,17 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsInt, IsOptional, Max, Min } from 'class-validator';
 
 export class PaginationQueryDto {
-  @ApiPropertyOptional()
   @IsInt()
   @Min(1)
   @IsOptional()
   @Type(() => Number)
-  readonly page: number = 1;
+  readonly page?: number = 1;
 
-  @ApiPropertyOptional()
   @IsInt()
   @Min(1)
-  @Max(10)
+  @Max(100)
   @IsOptional()
   @Type(() => Number)
-  readonly limit: number = 10;
+  readonly limit?: number = 10;
 }
